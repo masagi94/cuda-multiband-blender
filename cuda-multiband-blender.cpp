7de01dee54a11dcd5d55312f1ef50a132acb1271
mauro <masagi94@gmail.com>
2025-09-19 19:22:11 -0400

    Initial commit: CUDA multiband blender skeleton for CoAspire review :)

---
+++ cuda-multiband-blender.cpp
@@ -0,0 +1,34 @@
+﻿// cuda-multiband-blender.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/cudawarping.hpp>
+#include <opencv2/cudafeatures2d.hpp>
+#include <opencv2/core/cuda.hpp>
+#include <opencv2/cudaimgproc.hpp>
+#include <opencv2/cudafilters.hpp>
+#include <opencv2/stitching/detail/warpers.hpp>
+#include <opencv2/stitching/detail/matchers.hpp>
+#include <opencv2/cudaarithm.hpp>
+
+int main()
+{
+    std::cout << "hello world! gonna blend some stuff later";
+}
+
+
+
+
+
+
+
+
+
+
+
+
