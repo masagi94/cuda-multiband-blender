918aa51de722c7d8b9a9f71863c7580eaf48f70f
mauro <masagi94@gmail.com>
2025-09-19 19:28:51 -0400

    Checking for CUDA devices

--- cuda-multiband-blender.cpp
+++ cuda-multiband-blender.cpp
@@ -18,7 +18,18 @@
 
 int main()
 {
-    std::cout << "hello world! gonna blend some stuff later";
+    std::cout << "hello world! gonna blend some stuff later\n\n";
+
+    int n = cv::cuda::getCudaEnabledDeviceCount();
+    std::cout << "CUDA devices seen by OpenCV: " << n << "\n";
+    if (n > 0) {
+        cv::cuda::DeviceInfo info(0);
+        std::cout << "Using device 0: " << info.name() << "\n";
+    }
+    else {
+        std::cout << "No CUDA device detected (or CUDA disabled in your OpenCV build).\n";
+    }
+
 }
 
 
