c4e0529156b5e5a87589de038f83fa78f67d856f
mauro <masagi94@gmail.com>
2025-09-24 17:29:35 -0400

    feat(MultibandBlender): add functionality for remaining methods.
    blend(): combine gaussian and laplacian outputs into single blended image
    blendPyramids(): blend laplacian pyramids with gradient masks
    reconstructFromLaplacianPyramid(): reconstruct final image by adding all pyramid levels
    
    saveGaussLapPyramids(): save laplacian and gaussian pyramids for the images and masks to verify functionality

--- src/MultibandBlender.hpp
+++ src/MultibandBlender.hpp
@@ -10,15 +10,12 @@ public:
 
     // Blend two images with a mask
     cv::Mat blend(const cv::Mat& img1, const cv::Mat& img2, const cv::Mat& mask);
+    std::vector<cv::cuda::GpuMat> buildGaussianPyramid(const cv::cuda::GpuMat& img, int levels, cv::cuda::Stream& s);
+    std::vector<cv::cuda::GpuMat> buildLaplacianPyramid(const std::vector<cv::cuda::GpuMat>& gauss);
 
-//private:
+private:
     int levels_;
 
-    // CUDA helper functions
-    std::vector<cv::cuda::GpuMat> buildGaussianPyramid(const cv::cuda::GpuMat& img, int levels);
-    std::vector<cv::cuda::GpuMat> buildLaplacianPyramid(const std::vector<cv::cuda::GpuMat>& gauss);
-    std::vector<cv::cuda::GpuMat> blendPyramids(const std::vector<cv::cuda::GpuMat>& lap1, 
-                                                      const std::vector<cv::cuda::GpuMat>& lap2, 
-                                                      const std::vector<cv::cuda::GpuMat>& mask_gauss);
+    std::vector<cv::cuda::GpuMat> blendPyramids(const std::vector<cv::cuda::GpuMat>& lap1, const std::vector<cv::cuda::GpuMat>& lap2, const std::vector<cv::cuda::GpuMat>& mask_gauss);
     cv::cuda::GpuMat reconstructFromLaplacianPyramid(const std::vector<cv::cuda::GpuMat>& lap);
 };
--- src/cuda-multiband-blender.cpp
+++ src/cuda-multiband-blender.cpp
@@ -1,5 +1,4 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <opencv2/opencv.hpp>
 #include <filesystem>
 #include <string>
@@ -10,7 +9,7 @@
 namespace fs = std::filesystem;
 
 
-
+// Check if cuda is available. Need it to run the program
 static bool checkForCuda() {
 
     int n = cv::cuda::getCudaEnabledDeviceCount();
@@ -37,7 +36,7 @@ static cv::Mat loadImage(const fs::path& image_path) {
     return image;
 }
 
-
+// Use OpenCV's feather blender on the input images
 static cv::Mat runFeatherBlend(const cv::Mat& left_16s, const cv::Mat& right_16s, const cv::Mat& mask_left, const cv::Mat& mask_right, const cv::Rect& roi_location) {
     cv::detail::FeatherBlender feather_blender;
     
@@ -55,6 +54,48 @@ static cv::Mat runFeatherBlend(const cv::Mat& left_16s, const cv::Mat& right_16s
     return feather_output_8U;
 }
 
+// Save gauss and lap pyramids for evaluation
+static void saveGaussLapPyramids(MultibandBlender& custom_blender, cv::Mat& left_img, cv::Mat& right_img, cv::Mat& left_mask, fs::path& pyr_dir) {
+    cv::cuda::GpuMat left_img_gpu, left_mask_gpu;
+    left_img_gpu.upload(left_img);
+    left_mask_gpu.upload(left_mask);
+    cv::cuda::Stream s;
+
+    // save left image gauss pyramids
+    auto pyr_results = custom_blender.buildGaussianPyramid(left_img_gpu, 5, s);
+    for (int i = 0; i < pyr_results.size(); i++) {
+        fs::path out_path = pyr_dir / ("gauss_" + std::to_string(i) + ".png");
+        cv::Mat temp;
+        pyr_results[i].download(temp);
+        //cv::imshow(std::to_string(i), temp);
+        cv::imwrite(out_path.string(), temp);
+    }
+
+    
+    // save left image laplace pyramids
+    auto laplace_result = custom_blender.buildLaplacianPyramid(pyr_results);
+    for (int i = 0; i < laplace_result.size(); i++) {
+        fs::path out_path = pyr_dir / ("lap_" + std::to_string(i) + ".png");
+        cv::Mat temp;
+        laplace_result[i].download(temp);
+        //cv::imshow(std::to_string(i), temp);
+        cv::imwrite(out_path.string(), temp);
+
+    }
+
+    // save left mask gauss pyramids
+    auto mask_pyr_results = custom_blender.buildGaussianPyramid(left_mask_gpu, 5, s);
+    for (int i = 0; i < mask_pyr_results.size(); i++) {
+        fs::path out_path = pyr_dir / ("gauss_mask_" + std::to_string(i) + ".png");
+        cv::Mat temp;
+        mask_pyr_results[i].download(temp);
+        //cv::imshow(std::to_string(i), temp);
+        cv::imwrite(out_path.string(), temp);
+    }
+
+    std::cout << "Saved pyramids to: " << pyr_dir << "\n";
+}
+
 
 
 int main()
@@ -64,8 +105,7 @@ int main()
         return EXIT_FAILURE;
     }
     
-
-    /* Set up paths for inputs and outputs */
+    // Set up paths for inputs and outputs 
     fs::path data_dir = "data";    
     fs::path left_img_path = data_dir / "f16_left.png";
     fs::path right_img_path = data_dir / "f16_right.png";
@@ -75,10 +115,13 @@ int main()
     fs::create_directories(results_dir);
     fs::path multiband_result_path = results_dir / "multiband_blend.png";
     fs::path feather_result_path = results_dir / "feather_blend.png";
-    fs::path mask_result_p = results_dir / "mask.png";
+    fs::path mask_result_path = results_dir / "mask.png";
+    fs::path pyr_dir = results_dir / "pyramids";
+    fs::create_directories(pyr_dir);
 
 
-    /* Load images, check size */
+
+    // Load images, check size 
     cv::Mat left_img = loadImage(left_img_path);
     cv::Mat right_img = loadImage(right_img_path);
     
@@ -86,74 +129,40 @@ int main()
         cv::resize(right_img, right_img, left_img.size());
     }
     
-    /* Create masks */
-    cv::Mat mask_left, mask_right;
-    const int overlap_cols = 100;
-    blend::masks::makeSoftMasks(left_img, right_img, mask_left, mask_right, overlap_cols);
+    // Create masks
+    cv::Mat left_mask, right_mask;
+    const int overlap_cols = 20;
+    blend::masks::makeSoftMasks(left_img, right_img, left_mask, right_mask, overlap_cols);
 
-    /* Create ROI */
+    // Create ROI
     const int width = left_img.cols;
     const int height = left_img.rows;
     cv::Rect roi_location(cv::Point(0, 0), cv::Size(width, height));
 
-    /* Prepare inputs for blending */
+    // Brighten one image to make blending more difficult
     const double alpha = 1.5;
     const double beta = 0;
     right_img.convertTo(right_img, -1, alpha, beta);
 
 
-
-    MultibandBlender custom_blender;
-
-    cv::cuda::GpuMat left_img_gpu;
-    std::vector< cv::cuda::GpuMat> pyr_results;
-    left_img_gpu.upload(left_img);
-    pyr_results = custom_blender.buildGaussianPyramid(left_img_gpu, 5);
-
-    std::cout << "*** pyr size: " << pyr_results.size();
-
-    for (int i = 0; i < pyr_results.size(); i++) {
-        cv::Mat temp;
-        pyr_results[i].download(temp);
-        cv::imshow(std::to_string(i), temp);
-    }
-    cv::waitKey(0);
-
-    /*auto laplac_result = custom_blender.buildLaplacianPyramid(pyr_results);
+    // Run custom blender, save output
+    MultibandBlender custom_blender(8);
+    cv::Mat overlap_blended = custom_blender.blend(left_img, right_img, left_mask);
+    saveGaussLapPyramids(custom_blender, left_img, right_img, left_mask, pyr_dir);
+    cv::imwrite(multiband_result_path.string(), overlap_blended);
+    cv::imshow("Custom blender", overlap_blended);
     
-    std::cout << "*** pyr size: " << laplac_result.size();
-
-    for (int i = 0; i < laplac_result.size(); i++) {
-        cv::Mat temp;
-        laplac_result[i].download(temp);
-        cv::imshow(std::to_string(i), temp);
-    }
-    cv::waitKey(0);*/
-
-
-
-
+    // Prepare inputs for Feather Blender
     cv::Mat left_img_16s, right_img_16s;
     left_img.convertTo(left_img_16s, CV_16SC3);
     right_img.convertTo(right_img_16s, CV_16SC3);
 
-    cv::Mat feather_output = runFeatherBlend(left_img_16s, right_img_16s, mask_left, mask_right, roi_location);
-
+    // Run feather blender, save output
+    cv::Mat feather_output = runFeatherBlend(left_img_16s, right_img_16s, left_mask, right_mask, roi_location);
+    cv::imwrite(feather_result_path.string(), feather_output);
+    cv::imwrite(mask_result_path.string(), left_mask);
     cv::imshow("OpenCV Feather", feather_output);
     cv::waitKey(0);
-
-    cv::imwrite(feather_result_path.string(), feather_output);
-    cv::imwrite(mask_result_p.string(), mask_left);
 }
-
-
-
-
-
-
-
-
-
-
 
 
