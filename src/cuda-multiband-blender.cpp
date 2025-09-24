1bb80d97f2d8233b530c587391e3ff008b0c1212
mauro <masagi94@gmail.com>
2025-09-24 11:23:38 -0400

    feat(feather_blend): add feather blend functionality
    - checkForCuda(): moved cuda check to separate function
    - loadImages(): load images in separate function
    - testFeatherBlend(): implement feather blend test with input images

--- src/cuda-multiband-blender.cpp
+++ src/cuda-multiband-blender.cpp
@@ -1,11 +1,13 @@
 ﻿
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <filesystem>
+
 #include "MultibandBlender.h"
 
+namespace fs = std::filesystem;
 
-
-void buildCosineMasks(int width, int height, int start_col, int end_col, cv::Mat& mask_left, cv::Mat& mask_right) {
+static void buildCosineMasks(int width, int height, int start_col, int end_col, cv::Mat& mask_left, cv::Mat& mask_right) {
     
     mask_left.create(height, width, CV_8UC1);
     const int span = std::max(1, end_col - start_col);
@@ -37,7 +39,7 @@ void buildCosineMasks(int width, int height, int start_col, int end_col, cv::Mat
 }
 
 // Returns the midpoint between the last non-black col of left image and first non-black of the right image.
-int findSeamCenterCol(const cv::Mat& img_left, const cv::Mat& img_right, int non_black_threshold = 1) {
+static int findSeamCenterCol(const cv::Mat& img_left, const cv::Mat& img_right, int non_black_threshold = 1) {
     
     CV_Assert(img_left.size() == img_right.size() && img_left.type() == CV_8UC3 && img_right.type() == CV_8UC3);
     const int width = img_left.cols;
@@ -82,9 +84,7 @@ static void makeSoftMasks(const cv::Mat& img_left, const cv::Mat& img_right, cv:
     buildCosineMasks(width, height, start_col, end_col, mask_left, mask_right);
 }
 
-
-int main()
-{
+static void checkForCuda() {
     std::cout << "hello world! gonna blend some stuff later\n\n";
 
     int n = cv::cuda::getCudaEnabledDeviceCount();
@@ -97,28 +97,89 @@ int main()
         std::cout << "No CUDA device detected. Terminating project...\n";
         exit(0);
     }
+}
 
-
-    cv::Mat left_img = cv::imread("data/f16_left.png", cv::IMREAD_COLOR);
-    cv::Mat right_img = cv::imread("data/f16_right.png", cv::IMREAD_COLOR);
-    //cv::Mat right_img = cv::imread("data/goose.png", cv::IMREAD_COLOR);
-
-        
-    cv::imshow("left_img", left_img);
-    cv::imshow("right_img", right_img);
+// Loads input images
+static cv::Mat loadImage(const fs::path& image_path) {
     
+    cv::Mat image = cv::imread(image_path.string(), cv::IMREAD_COLOR);
+
+    if (image.empty())
+        CV_Error(cv::Error::StsError, cv::format("\n\nFailed to load: %s", image_path.string().c_str()));
+    
+    return image;
+}
+
+
+static cv::Mat testFeatherBlend(cv::Mat& left_16s, cv::Mat& right_16s, cv::Mat& mask_left, cv::Mat& mask_right, cv::Rect& roi_location) {
+    cv::detail::FeatherBlender feather_blender;
+    
+    feather_blender.prepare(roi_location);
+    feather_blender.feed(left_16s, mask_left, cv::Point(0, 0));
+    feather_blender.feed(right_16s, mask_right, cv::Point(0, 0));
+
+    cv::Mat feather_output, feather_mask;
+    
+    feather_blender.blend(feather_output, feather_mask);
+    
+    cv::Mat feather_output_8U;
+    feather_output.convertTo(feather_output_8U, CV_8U);
+
+    return feather_output_8U;
+}
+
+
+int main()
+{
+    checkForCuda();
+
+    /* Set up paths for inputs and outputs */
+    fs::path data_dir = "data";    
+    fs::path left_img_path = data_dir / "f16_left.png";
+    fs::path right_img_path = data_dir / "f16_right.png";
+    //fs::path right_p = data_dir / "data/goose.png";
+
+    fs::path results_dir = "results";
+    fs::create_directories(results_dir);
+    fs::path multiband_result_path = results_dir / "multiband_blend.png";
+    fs::path feather_result_path = results_dir / "feather_blend.png";
+    fs::path mask_result_p = results_dir / "mask.png";
+
+
+    /* Load images, check size */
+    cv::Mat left_img = loadImage(left_img_path);
+    cv::Mat right_img = loadImage(right_img_path);
+    
+    if (left_img.size() != right_img.size()) {
+        cv::resize(right_img, right_img, left_img.size());
+    }
+    
+    /* Create masks */
     cv::Mat mask_left, mask_right;
-    makeSoftMasks(left_img, right_img, mask_left, mask_right, 10);
+    const int overlap_cols = 100;
+    makeSoftMasks(left_img, right_img, mask_left, mask_right, overlap_cols);
 
+    /* Create ROI */
+    const int width = left_img.cols;
+    const int height = left_img.rows;
+    cv::Rect roi_location(cv::Point(0, 0), cv::Size(width, height));
 
-    cv::imshow("mask_right", mask_right);
+    /* Prepare inputs for blending */
+    const double alpha = 1.5;
+    const double beta = 0;
+    right_img.convertTo(right_img, -1, alpha, beta);
+
+    cv::Mat left_img_16s, right_img_16s;
+    left_img.convertTo(left_img_16s, CV_16SC3);
+    right_img.convertTo(right_img_16s, CV_16SC3);
+
+    cv::Mat feather_output = testFeatherBlend(left_img_16s, right_img_16s, mask_left, mask_right, roi_location);
+
+    cv::imshow("OpenCV Feather", feather_output);
     cv::waitKey(0);
 
-
-    
-
-
-
+    cv::imwrite(feather_result_path.string(), feather_output);
+    cv::imwrite(mask_result_p.string(), mask_left);
 }
 
 
